5de8bee0c749b243c860c1b84c44ba7cd11d444a
Ronny Presque <Ronneeh@gmail.com>
2025-11-10 20:43:25 +0000

    mais compacto por favor XD

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -3,9 +3,9 @@ import { Icons } from "@/components/icons";
 export default function Footer() {
   return (
     <footer className="bg-secondary">
-      <div className="container flex flex-col items-center justify-between gap-4 py-6 md:h-20 md:flex-row md:py-0">
-        <div className="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
-          <Icons.Logo className="h-6 w-6 text-accent" />
+      <div className="container flex flex-col items-center justify-between gap-2 py-3 md:h-16 md:flex-row md:py-0">
+        <div className="flex flex-col items-center gap-2 px-8 md:flex-row md:gap-2 md:px-0">
+          <Icons.Logo className="h-5 w-5 text-accent" />
           <p className="text-center text-sm leading-loose text-muted-foreground md:text-left">
             Built by your friendly neighborhood AI.
           </p>
