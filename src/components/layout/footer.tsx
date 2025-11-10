577099a890030f7bbd3bc84fe3b9c2bf1c8d408e
Ronny Presque <Ronneeh@gmail.com>
2025-11-10 20:41:14 +0000

    vamos tentar por um degrade no footer para eu testar se fica visualmente

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -2,7 +2,7 @@ import { Icons } from "@/components/icons";
 
 export default function Footer() {
   return (
-    <footer className="bg-secondary">
+    <footer className="bg-gradient-to-t from-background to-secondary">
       <div className="container flex flex-col items-center justify-between gap-4 py-10 md:h-24 md:flex-row md:py-0">
         <div className="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
           <Icons.Logo className="h-6 w-6 text-accent" />
