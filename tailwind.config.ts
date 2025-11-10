2fd9a5305e83944953384f6a51eeea28c029d5fb
Ronny Presque <Ronneeh@gmail.com>
2025-11-10 21:11:09 +0000

    era mesmo azul, voce não entende o conceito0 de azul playstation né? hah

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -55,6 +55,7 @@ export default {
         border: 'hsl(var(--border))',
         input: 'hsl(var(--input))',
         ring: 'hsl(var(--ring))',
+        'playstation-blue': 'hsl(var(--playstation-blue))',
         chart: {
           '1': 'hsl(var(--chart-1))',
           '2': 'hsl(var(--chart-2))',
