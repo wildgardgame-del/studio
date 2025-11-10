8a177e110e2b516edfb49fcf40f8cc46ea0d1260
Ronny Presque <Ronneeh@gmail.com>
2025-11-10 22:02:35 +0000

    Try fixing this error: `Runtime Error: Error: Invalid src prop (https://

--- next.config.ts
+++ next.config.ts
@@ -47,6 +47,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'files.catbox.moe',
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
 };
