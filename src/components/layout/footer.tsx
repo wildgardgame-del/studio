aaf1751ef3ed4b8522ce57f81ef653024be77554
Ronny Presque <Ronneeh@gmail.com>
2025-11-10 20:42:51 +0000

    esquece, volte com cor solida e pode deixar ele mais estreito verticalme

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -2,8 +2,8 @@ import { Icons } from "@/components/icons";
 
 export default function Footer() {
   return (
-    <footer className="bg-gradient-to-b from-secondary to-background">
-      <div className="container flex flex-col items-center justify-between gap-4 py-10 md:h-24 md:flex-row md:py-0">
+    <footer className="bg-secondary">
+      <div className="container flex flex-col items-center justify-between gap-4 py-6 md:h-20 md:flex-row md:py-0">
         <div className="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
           <Icons.Logo className="h-6 w-6 text-accent" />
           <p className="text-center text-sm leading-loose text-muted-foreground md:text-left">
