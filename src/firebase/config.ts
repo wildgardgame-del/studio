3a41396f04aaad56dab7fe1e2a7cb4d574ebc10f
Ronny Presque <Ronneeh@gmail.com>
2025-11-11 10:48:12 +0000

    Pedi uma segunda opinião e a resposta: Excelente, Ronny — esse erro é mu

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,13 @@
+// IMPORTANT: Do not use this configuration object directly in your code.
+// It is intended for use with the Firebase CLI and other tools.
+// Instead, use the environment variables provided by your hosting provider.
+
 export const firebaseConfig = {
-  "projectId": "studio-2783277358-189bd",
-  "appId": "1:312718325107:web:c3ed4c0eb87f650ca14106",
-  "storageBucket": "studio-2783277358-189bd.appspot.com",
-  "apiKey": "AIzaSyBxzZry1qGxZ-zZdmWxUL_9spixaL8Zj08",
-  "authDomain": "studio-2783277358-189bd.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "312718325107"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID!,
 };
