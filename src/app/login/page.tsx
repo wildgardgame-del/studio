47ee12e709a3f5aeffefe290335c70206e415056
Ronny Presque <Ronneeh@gmail.com>
2025-11-11 12:18:50 +0000

    esta dando problema no login por aqui tambem

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -116,12 +116,6 @@ export default function LoginPage() {
                           Login with Google
                       </Button>
                     </div>
-                    <div className="mt-4 text-center text-sm">
-                      Don't have an account?{" "}
-                      <Link href="/signup" className="underline">
-                          Sign up
-                      </Link>
-                    </div>
                 </CardContent>
             </Card>
         </main>
